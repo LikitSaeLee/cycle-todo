a99821d7765e0caea80bda40f2a010428d6c80d1
Likit Sae Lee <likit.usm@gmail.com>
2017-10-01 22:11:10 +0800

    got bug

--- src/app.js
+++ src/app.js
@@ -1,12 +1,60 @@
-import {div} from '@cycle/dom'
+import {div, input, p, button} from '@cycle/dom'
 import xs from 'xstream'
 
-export function App (sources) {
-  const vtree$ = xs.of(
-    div('My Awesome Cycle.js app')
+function intent(domSource) {
+  const inputTodo$ = domSource.
+    select('#todo-input').
+    events('input');
+
+  const clickAddTodo$ = domSource.
+    select('#add-todo-btn').
+    events('click');
+
+  return { inputTodo$, clickAddTodo$ };
+}
+
+function model(actions) {
+  const { inputTodo$, clickAddTodo$ } = actions;
+
+  const todo$ = inputTodo$
+    .map(ev => ev.target.value)
+    .startWith('');
+
+  const addTodo$ = clickAddTodo$
+    .map(() => todo$.last())
+    .startWith('Add a todo now.');
+
+  return { todo$, addTodo$ };
+}
+
+function view(state) {
+  const { addTodo$, todo$ } = state;
+
+  return xs.combine(addTodo$, todo$).map(([addTodo, todo]) =>
+    div([
+      input('#todo-input'),
+      button('#add-todo-btn', 'Add Todo'),
+      p(todo),
+      p(addTodo),
+    ])
   )
+
+}
+
+export function App (sources) {
+  const actions = intent(sources.DOM);
+  const state = model(actions);
+  const vtree$ = view(state);
+
   const sinks = {
-    DOM: vtree$
+    DOM: vtree$,
+    Log: (addTodo$) => {
+      addTodo$.subscribe({
+        next: (todo) => { console.log(`New Todo: ${todo}`) },
+        error: (err) => {},
+        completed: () => {},
+      });
+    }
   }
   return sinks
 }
