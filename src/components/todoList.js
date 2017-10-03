701dc98a3d41e01270d847eecc474c984c5ad829
Likit Sae Lee <likit.usm@gmail.com>
2017-10-03 13:14:28 +0800

    refactor todoList to mvi

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -6,31 +6,48 @@ function renderTodoList(todoes) {
   return div(todoes.filter(todo => todo).map(todo => p('.todo-item', todo)))
 }
 
-export default function todoList(sources) {
-  const { todo$ } = sources.props;
-
-  const deleteTodo$ = sources
-    .DOM
+function intent(domSource) {
+  const deleteTodo$ = domSource
     .select('.todo-item')
     .events('click')
     .map(ev => ev.target.textContent)
     .startWith([]);
 
+  return { deleteTodo$ }
+}
+
+function model(actions, todo$) {
+
+  const { deleteTodo$ } = actions;
+
   const deletedTodoes$ = deleteTodo$
     .fold((todoes, todo) => [...todoes, todo], []);
 
   const todoes$ = todo$
-    .fold((todoes, todo) => [...todoes, todo], [])
+    .fold((todoes, todo) => [...todoes, todo], []);
 
-  const vdom$ = xs
+  return { deletedTodoes$, todoes$ }
+}
+
+function view(state) {
+  const {todoes$, deletedTodoes$} = state;
+
+  return xs
     .combine(todoes$, deletedTodoes$)
     .map(([todoes, deletedTodoes]) =>
       todoes.filter(todo => !deletedTodoes.includes(todo))
     )
     .map(todoes => renderTodoList(todoes));
+}
+
+export default function todoList(sources) {
+  const { todo$ } = sources.props;
+
+  const actions = intent(sources.DOM);
+  const state = model(actions, todo$);
+  const vdom$ = view(state);
 
   return {
-    DOM: vdom$,
-    deleteTodo$,
+    DOM: vdom$
   }
 }
