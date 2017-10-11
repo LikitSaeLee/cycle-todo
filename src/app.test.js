b96b3c214afee61064db7a5ba4c0b81ade333d0e
Likit Sae Lee <likit.usm@gmail.com>
2017-10-11 22:57:40 +0800

    wip app test

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,42 @@
- import assert from 'assert'
+import { mockDOMSource } from '@cycle/dom';
+import { mockTimeSource } from '@cycle/time';
+import { select } from 'snabbdom-selector';
+import { expect } from 'chai';
 
- describe('App', function () {
-   it('check 1+1 eq 2', function () {
-     assert.equal(1+1, 2);
-   })
- })
+import App from './app';
+
+describe('App', function() {
+  it('renders todoes', function(done) {
+    const Time = mockTimeSource();
+
+    const storage = Time.diagram(
+      '-x--------|',
+      {
+        x: JSON.stringify([
+          { title: 'todo 1', completed: false },
+          { title: 'todo 2', completed: false }
+        ])
+      }
+    )
+
+    const expectedTodoList$ = Time.diagram(
+      'x|',
+      {
+        x: ['todo 1', 'todo 2']
+      }
+    )
+    const DOM = mockDOMSource()
+
+
+    const AppSink = App({DOM, storage})
+
+    const todoList$ = AppSink
+      .DOM
+      .map(vtree =>
+        select('.todo-item').map(vNode => vNode.text)
+      )
+
+    Time.assertEqual(todoList$, expectedTodoList$);
+    Time.run(done)
+  })
+})
